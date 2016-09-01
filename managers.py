1563bae0633534000b725ddd73d3dc8dc2aba837
RichterSR <sven.richter@infotec-richter.de>
2016-09-01 09:46:44 +0200

    Add files via upload

---
+++ managers.py
@@ -0,0 +1,218 @@
+from django.contrib.auth.models import User
+
+from models import AuthServicesInfo
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class AuthServicesInfoManager:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def __get_or_create(user):
+        if AuthServicesInfo.objects.filter(user=user).exists():
+            logger.debug("Returning existing authservicesinfo model for user %s" % user)
+            return AuthServicesInfo.objects.get(user=user)
+        else:
+            # We have to create
+            logger.info("Creating new authservicesinfo model for user %s" % user)
+            authserviceinfo = AuthServicesInfo()
+            authserviceinfo.user = user
+            authserviceinfo.save()
+            return authserviceinfo
+
+    @staticmethod
+    def get_auth_service_info(user):
+        if User.objects.filter(username=user.username).exists():
+            return AuthServicesInfoManager.__get_or_create(user)
+        logger.error("Failed to get authservicesinfo object for user %s: user does not exist." % user)
+        return None
+
+    @staticmethod
+    def update_main_char_Id(char_id, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s main character to id %s" % (user, char_id))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.main_char_id = char_id
+            authserviceinfo.save(update_fields=['main_char_id'])
+            logger.info("Updated user %s main character to id %s" % (user, char_id))
+        else:
+            logger.error("Failed to update user %s main character id to %s: user does not exist." % (user, char_id))
+
+    @staticmethod
+    def update_user_forum_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s forum info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.forum_username = username
+            authserviceinfo.forum_password = password
+            authserviceinfo.save(update_fields=['forum_username', 'forum_password'])
+            logger.info("Updated user %s forum info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s forum info: user does not exist." % user)
+
+    @staticmethod
+    def update_user_jabber_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s jabber info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.jabber_username = username
+            authserviceinfo.jabber_password = password
+            authserviceinfo.save(update_fields=['jabber_username', 'jabber_password'])
+            logger.info("Updated user %s jabber info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s jabber info: user does not exist." % user)
+
+
+    @staticmethod
+    def update_user_mumble_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s mumble info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.mumble_username = username
+            authserviceinfo.mumble_password = password
+            authserviceinfo.save(update_fields=['mumble_username', 'mumble_password'])
+            logger.info("Updated user %s mumble info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s mumble info: user does not exist." % user)
+
+    @staticmethod
+    def update_user_ipboard_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s ipboard info: uername %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.ipboard_username = username
+            authserviceinfo.ipboard_password = password
+            authserviceinfo.save(update_fields=['ipboard_username', 'ipboard_password'])
+            logger.info("Updated user %s ipboard info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s ipboard info: user does not exist." % user)
+
+    @staticmethod
+    def update_user_xenforo_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s xenforo info: uername %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.xenforo_username = username
+            authserviceinfo.xenforo_password = password
+            authserviceinfo.save(update_fields=['xenforo_username', 'xenforo_password'])
+            logger.info("Updated user %s xenforo info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s xenforo info: user does not exist." % user)
+
+    @staticmethod
+    def update_user_teamspeak3_info(uid, perm_key, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s teamspeak3 info: uid %s" % (user, uid))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.teamspeak3_uid = uid
+            authserviceinfo.teamspeak3_perm_key = perm_key
+            authserviceinfo.save(update_fields=['teamspeak3_uid', 'teamspeak3_perm_key'])
+            logger.info("Updated user %s teamspeak3 info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s teamspeak3 info: user does not exist." % user)
+
+    @staticmethod
+    def update_is_blue_10(is_blue_10, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s blue 10 status: %s" % (user, is_blue_10))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.is_blue_10 = is_blue_10
+            authserviceinfo.save(update_fields=['is_blue_10'])
+            logger.info("Updated user %s blue 10 status to %s in authservicesinfo model." % (user, is_blue_10))
+
+
+    @staticmethod
+    def update_is_blue(is_blue, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s blue status: %s" % (user, is_blue))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.is_blue = is_blue
+            authserviceinfo.save(update_fields=['is_blue'])
+            logger.info("Updated user %s blue status to %s in authservicesinfo model." % (user, is_blue))
+
+    @staticmethod
+    def update_user_discord_info(user_id, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s discord info: user_id %s" % (user, user_id))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.discord_uid = user_id
+            authserviceinfo.save(update_fields=['discord_uid'])
+            logger.info("Updated user %s discord info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s discord info: user does not exist." % user)
+            
+    @staticmethod
+    def update_user_discourse_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s discourse info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.discourse_username = username
+            authserviceinfo.discourse_password = password
+            authserviceinfo.save(update_fields=['discourse_username', 'discourse_password'])
+            logger.info("Updated user %s discourse info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s discourse info: user does not exist." % user)
+
+    @staticmethod
+    def update_user_ips4_info(username, password, id, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s IPS4 info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.ips4_username = username
+            authserviceinfo.ips4_password = password
+            authserviceinfo.ips4_id = id
+            authserviceinfo.save(update_fields=['ips4_username', 'ips4_password', 'ips4_id'])
+            logger.info("Updated user %s IPS4 info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s IPS4 info: user does not exist." % user)
+    
+    @staticmethod  
+    def update_user_seat_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s SeAT info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.seat_username = username
+            authserviceinfo.seat_password = password
+            authserviceinfo.save(update_fields=['seat_username', 'seat_password'])
+            logger.info("Updated user %s SeAT info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s SeAT info: user does not exist." % user)
+
+    @staticmethod
+    def update_user_smf_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s forum info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.smf_username = username
+            authserviceinfo.smf_password = password
+            authserviceinfo.save(update_fields=['smf_username', 'smf_password'])
+            logger.info("Updated user %s smf info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s smf info: user does not exist." % user)
+
+    @staticmethod
+    def update_user_market_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s market info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.market_username = username
+            authserviceinfo.market_password = password
+            authserviceinfo.save(update_fields=['market_username', 'market_password'])
+            logger.info("Updated user %s market info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s market info: user does not exist." % user)
+
+    @staticmethod
+    def update_user_pathfinder_info(username, password, user):
+        if User.objects.filter(username=user.username).exists():
+            logger.debug("Updating user %s market info: username %s" % (user, username))
+            authserviceinfo = AuthServicesInfoManager.__get_or_create(user)
+            authserviceinfo.pathfinder_username = username
+            authserviceinfo.pathfinder_password = password
+            authserviceinfo.save(update_fields=['pathfinder_username', 'pathfinder_password'])
+            logger.info("Updated user %s pathfinder info in authservicesinfo model." % user)
+        else:
+            logger.error("Failed to update user %s pathfinder info: user does not exist." % user)
---
+++ views.py
@@ -0,0 +1,278 @@
+from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import HttpResponseRedirect
+
+from collections import namedtuple
+
+from authentication.managers import AuthServicesInfoManager
+from services.managers.eve_api_manager import EveApiManager
+from services.managers.evewho_manager import EveWhoManager
+from eveonline.models import EveCorporationInfo
+from eveonline.models import EveAllianceInfo
+from eveonline.models import EveCharacter
+from eveonline.models import EveApiKeyPair
+from fleetactivitytracking.models import Fat
+from util import check_if_user_has_permission
+from forms import CorputilsSearchForm
+from evelink.api import APIError
+
+import logging
+import datetime
+
+logger = logging.getLogger(__name__)
+class Player(object):
+    def __init__(self, main, user, maincorp, maincorpid, altlist, apilist, n_fats):
+        self.main = main
+        self.user = user
+        self.maincorp = maincorp
+        self.maincorpid = maincorpid
+        self.altlist = altlist
+        self.apilist = apilist
+        self.n_fats = n_fats
+
+def first_day_of_next_month(year, month):
+    if month == 12:
+        return datetime.datetime(year+1,1,1)
+    else:
+        return datetime.datetime(year, month+1, 1)
+
+def first_day_of_previous_month(year, month):
+    if month == 1:
+        return datetime.datetime(year-1,12,1)
+    else:
+        return datetime.datetime(year, month-1, 1)
+
+
+@login_required
+def corp_member_view(request, corpid = None, year=datetime.date.today().year, month=datetime.date.today().month):
+    year = int(year)
+    month = int(month)
+    start_of_month = datetime.datetime(year, month, 1)
+    start_of_next_month = first_day_of_next_month(year, month)
+    start_of_previous_month = first_day_of_previous_month(year, month)
+    logger.debug("corp_member_view called by user %s" % request.user)
+
+    try:
+        user_main = EveCharacter.objects.get(character_id=AuthServicesInfoManager.get_auth_service_info(user=request.user).main_char_id)
+        user_corp_id = int(user_main.corporation_id)
+    except (ValueError, EveCharacter.DoesNotExist):
+        user_corp_id = settings.CORP_ID
+
+
+    if not settings.IS_CORP:
+        alliance = EveAllianceInfo.objects.get(alliance_id=settings.ALLIANCE_ID)
+        alliancecorps = EveCorporationInfo.objects.filter(alliance=alliance)
+        membercorplist = [(int(membercorp.corporation_id), str(membercorp.corporation_name)) for membercorp in alliancecorps]
+        membercorplist.sort(key=lambda tup: tup[1])
+        membercorp_id_list = [int(membercorp.corporation_id) for membercorp in alliancecorps]
+
+        bluecorps = EveCorporationInfo.objects.filter(is_blue=True)
+        bluecorplist = [(int(bluecorp.corporation_id), str(bluecorp.corporation_name)) for bluecorp in bluecorps]
+        bluecorplist.sort(key=lambda tup: tup[1])
+        bluecorp_id_list = [int(bluecorp.corporation_id) for bluecorp in bluecorps]
+
+        if not (user_corp_id in membercorp_id_list or user_corp_id not in bluecorp_id_list):
+            user_corp_id = None
+
+    if not corpid:
+        if(settings.IS_CORP):
+            corpid = settings.CORP_ID
+        elif user_corp_id:
+            corpid = user_corp_id
+        else:
+            corpid = membercorplist[0][0]
+
+    corp = EveCorporationInfo.objects.get(corporation_id=corpid)
+
+    if check_if_user_has_permission(request.user, 'alliance_apis') or (check_if_user_has_permission(request.user, 'corp_apis') and (user_corp_id == corpid)):
+        logger.debug("Retreiving and sending API-information")
+
+        if settings.IS_CORP:
+            try:
+                member_list = EveApiManager.get_corp_membertracking(settings.CORP_API_ID, settings.CORP_API_VCODE)
+            except APIError:
+                logger.debug("Corp API does not have membertracking scope, using EveWho data instead.")
+                member_list = EveWhoManager.get_corporation_members(corpid)
+        else:
+            member_list = EveWhoManager.get_corporation_members(corpid)
+
+        characters_with_api = {}
+        characters_without_api = {}
+
+        num_registered_characters = 0
+        for char_id, member_data in member_list.items():
+            try:
+                char = EveCharacter.objects.get(character_id=char_id)
+                char_owner = char.user
+                try:
+                    mainid = int(AuthServicesInfoManager.get_auth_service_info(user=char_owner).main_char_id)
+                    mainchar = EveCharacter.objects.get(character_id=mainid)
+                    mainname = mainchar.character_name
+                    maincorp = mainchar.corporation_name
+                    maincorpid = mainchar.corporation_id
+                    api_pair = EveApiKeyPair.objects.get(api_id=char.api_id)
+                except (ValueError, EveCharacter.DoesNotExist, EveApiKeyPair.DoesNotExist):
+                    logger.info("No main character seem to be set for character %s" % char.character_name)
+                    mainname = "User: " + char_owner.username
+                    mainchar = char
+                    maincorp = "Not set."
+                    maincorpid = None
+                    api_pair = None
+                num_registered_characters = num_registered_characters + 1
+                characters_with_api.setdefault(mainname, Player(main=mainchar,
+                                                                user=char_owner,
+                                                                maincorp=maincorp,
+                                                                maincorpid=maincorpid,
+                                                                altlist=[],
+                                                                apilist=[],
+                                                                n_fats=0)
+                                               ).altlist.append(char)
+                if api_pair:
+                    characters_with_api[mainname].apilist.append(api_pair)
+
+            except (EveCharacter.DoesNotExist):
+                characters_without_api.update({member_data["name"]: member_data["id"]})
+
+        for char in EveCharacter.objects.filter(corporation_id=corpid):
+            if not int(char.character_id) in member_list:
+                logger.info("Character '%s' does not exist in EveWho dump." % char.character_name)
+                char_owner = char.user
+                try:
+                    mainid = int(AuthServicesInfoManager.get_auth_service_info(user=char_owner).main_char_id)
+                    mainchar = EveCharacter.objects.get(character_id=mainid)
+                    mainname = mainchar.character_name
+                    maincorp = mainchar.corporation_name
+                    maincorpid = mainchar.corporation_id
+                    api_pair = EveApiKeyPair.objects.get(api_id=char.api_id)
+                except (ValueError, EveCharacter.DoesNotExist, EveApiKeyPair.DoesNotExist):
+                    logger.info("No main character seem to be set for character %s" % char.character_name)
+                    mainname = "User: " + char_owner.username
+                    mainchar = char
+                    maincorp = "Not set."
+                    maincorpid = None
+                    api_pair = None
+                num_registered_characters = num_registered_characters + 1
+                characters_with_api.setdefault(mainname, Player(main=mainchar,
+                                                                user=char_owner,
+                                                                maincorp=maincorp,
+                                                                maincorpid=maincorpid,
+                                                                altlist=[],
+                                                                apilist=[],
+                                                                n_fats=0)
+                                               ).altlist.append(char)
+                if api_pair:
+                    characters_with_api[mainname].apilist.append(api_pair)
+
+        n_unacounted = corp.member_count - (num_registered_characters + len(characters_without_api))
+
+        for mainname, player in characters_with_api.items():
+            fats_this_month = Fat.objects.filter(user=player.user).filter(fatlink__fatdatetime__gte = start_of_month).filter(fatlink__fatdatetime__lt = start_of_next_month)
+            characters_with_api[mainname].n_fats = len(fats_this_month)
+
+        if start_of_next_month > datetime.datetime.now():
+            start_of_next_month = None
+
+
+        if not settings.IS_CORP:
+            context = {"membercorplist": membercorplist,
+                       "corp": corp,
+                       "characters_with_api": sorted(characters_with_api.items()),
+                       'n_registered': num_registered_characters,
+                       'n_unacounted': n_unacounted,
+                       "characters_without_api": sorted(characters_without_api.items()),
+                       "search_form": CorputilsSearchForm()}
+        else:
+            logger.debug("corp_member_view running in corportation mode")
+            context = {"corp": corp,
+                       "characters_with_api": sorted(characters_with_api.items()),
+                       'n_registered': num_registered_characters,
+                       'n_unacounted': n_unacounted,
+                       "characters_without_api": sorted(characters_without_api.items()),
+                       "search_form": CorputilsSearchForm()}
+
+        context["next_month"] = start_of_next_month
+        context["previous_month"] = start_of_previous_month
+        context["this_month"] = start_of_month
+
+        return render_to_response('registered/corputils.html',context, context_instance=RequestContext(request) )
+    return HttpResponseRedirect("/dashboard/")
+
+
+@login_required
+def corputils_search(request, corpid=settings.CORP_ID):
+    logger.debug("corputils_search called by user %s" % request.user)
+
+    corp = EveCorporationInfo.objects.get(corporation_id=corpid)
+
+    authorized = False
+    try:
+        user_main = EveCharacter.objects.get(character_id=AuthServicesInfoManager.get_auth_service_info(user=request.user).main_char_id)
+        if check_if_user_has_permission(request.user, 'alliance_apis') or (check_if_user_has_permission(request.user, 'corp_apis') and (user_main.corporation_id == corpid)):
+            logger.debug("Retreiving and sending API-information")
+            authorized = True
+    except (ValueError, EveCharacter.DoesNotExist):
+        if check_if_user_has_permission(request.user, 'alliance_apis'):
+            logger.debug("Retreiving and sending API-information")
+            authorized = True
+
+    if authorized:
+        if request.method == 'POST':
+            form = CorputilsSearchForm(request.POST)
+            logger.debug("Request type POST contains form valid: %s" % form.is_valid())
+            if form.is_valid():
+                # Really dumb search and only checks character name
+                # This can be improved but it does the job for now
+                searchstring = form.cleaned_data['search_string']
+                logger.debug("Searching for player with character name %s for user %s" % (searchstring, request.user))
+
+                if settings.IS_CORP:
+                    try:
+                        member_list = EveApiManager.get_corp_membertracking(settings.CORP_API_ID, settings.CORP_API_VCODE)
+                    except APIError:
+                        logger.debug("Corp API does not have membertracking scope, using EveWho data instead.")
+                        member_list = EveWhoManager.get_corporation_members(corpid)
+                else:
+                    member_list = EveWhoManager.get_corporation_members(corpid)
+
+                SearchResult = namedtuple('SearchResult', ['name', 'id', 'main', 'api_registered', 'character', 'apiinfo'])
+
+                searchresults = []
+                for memberid, member_data in member_list.items():
+                    if searchstring.lower() in member_data["name"].lower():
+                        try:
+                            char = EveCharacter.objects.get(character_name=member_data["name"])
+                            user = char.user
+                            mainid = int(AuthServicesInfoManager.get_auth_service_info(user=user).main_char_id)
+                            main = EveCharacter.objects.get(character_id=mainid)
+                            api_registered = True
+                            apiinfo = EveApiKeyPair.objects.get(api_id=char.api_id)
+                        except EveCharacter.DoesNotExist:
+                            api_registered = False
+                            char = None
+                            main = ""
+                            apiinfo = None
+
+                        searchresults.append(SearchResult(name=member_data["name"], id=memberid, main=main, api_registered=api_registered,
+                                                    character=char, apiinfo=apiinfo))
+
+
+                logger.info("Found %s members for user %s matching search string %s" % (len(searchresults), request.user, searchstring))
+
+                context = {'corp': corp, 'results': searchresults, 'search_form': CorputilsSearchForm(), "year":datetime.datetime.now().year, "month":datetime.datetime.now().month}
+
+                return render_to_response('registered/corputilssearchview.html',
+                                          context, context_instance=RequestContext(request))
+            else:
+                logger.debug("Form invalid - returning for user %s to retry." % request.user)
+                context = {'corp': corp, 'members': None, 'search_form': CorputilsSearchForm()}
+                return render_to_response('registered/corputilssearchview.html',
+                                          context, context_instance=RequestContext(request))
+
+        else:
+            logger.debug("Returning empty search form for user %s" % request.user)
+            return HttpResponseRedirect("/corputils/")
+    return HttpResponseRedirect("/dashboard/")
+
+
